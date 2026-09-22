899695b09f1a659777bdc021acff020dd7026241
AidenLetth <22112326@student.westernsydney.edu.au>
2026-09-22 15:21:37 +1000

    beta

--- Aiden/aiden/pipeline_stack.py
+++ Aiden/aiden/pipeline_stack.py
@@ -10,13 +10,17 @@ from .pipeline_stage import AidenPipelineStage
 class AidenPipelineStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-
+        
+        #https://docs.aws.amazon.com/cdk/api/v2/python/aws_cdk.pipelines/CodePipelineSource.html#aws_cdk.pipelines.CodePipelineSource.git_hub
+        #first step is to define the source of the pipeline, which is a GitHub repository 
+        #the authentication is done using a secret stored in AWS Secrets Manager
         source = pipeline_.CodePipelineSource.git_hub(
             repo_string="AidenLetth/WSU2026",
             branch="main",
             authentication=SecretValue.secrets_manager("githubSecret")
         )
-
+        #https://docs.aws.amazon.com/cdk/api/v2/python/aws_cdk.pipelines/ShellStep.html#aws_cdk.pipelines.ShellStep
+        #the second step is to define the synth step, which is a ShellStep that runs the commands to synthesize the CDK app
         synth = pipeline_.ShellStep(
             "Synth",
             input=source,
@@ -34,7 +38,8 @@ class AidenPipelineStack(Stack):
             "AidenPipeline",
             synth=synth
         )
-        #Alpha 
+        #Alpha Stage is a stage that runs the unit tests for the CDK app, it is added to the pipeline after the synth step
+        #https://docs.aws.amazon.com/cdk/api/v2/python/aws_cdk.pipelines/CodePipeline.html#aws_cdk.pipelines.CodePipeline.add_stage
         AlphaStage = AidenPipelineStage( self, "UnitTestStage")
         pipeline.add_stage (AlphaStage,
                             pre=[pipeline_.ShellStep("UnitTestBlocker",
@@ -43,8 +48,18 @@ class AidenPipelineStack(Stack):
                                    "python -m pip install -r requirements.txt",
                                    "python -m pip install pytest",
                                    "python3 -m pytest"
-                                   ]
-        )
+                                   ] )
                                 ]
                             )
-       
+        #Beta Stage is a stage that runs the integration tests for the CDK app, it is added to the pipeline after the Alpha Stage
+        BetaStage = AidenPipelineStage( self, "IntegrationTestStage")
+        pipeline.add_stage (BetaStage,
+                            pre=[pipeline_.ShellStep("IntegrationTestBlocker",
+                                 commands=[ "npm install -g aws-cdk",
+                                   "cd Aiden/",
+                                   "python -m pip install -r requirements.txt",
+                                   "python -m pip install pytest",
+                                   "python3 -m pytest"
+                                   ] )
+                                ]
+                            )
